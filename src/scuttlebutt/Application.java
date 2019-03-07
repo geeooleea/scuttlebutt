e8037cdbec7b0f4941fb980a96b009845b5a76ae
geeooleea <giulia.carocari@studenti.unitn.it>
2019-03-07 19:06:06 +0100

    Edit database update mechanism in Application class

--- src/scuttlebutt/Application.java
+++ src/scuttlebutt/Application.java
@@ -23,13 +23,15 @@ public class Application implements Control {
 
 	private static final String PAR_MIN_SIZE = "minsize";
 
+	private static final String PAR_MTU = "mtu";
+
 	private final int pid;
 
 	private static final int N = Network.size();
 
 	private static int K;
 
-	private boolean exec = false;
+	private static int MTU;
 
 	public static void setK(int K) {
 		Application.K = K;
@@ -40,31 +42,39 @@ public class Application implements Control {
 	public Application(String prefix) {
 		pid = Configuration.getPid(prefix + "." + PAR_PROT);
 		maxs = Configuration.getInt(prefix + "." + PAR_MAX_SIZE);
+		System.err.println("----> Maximum update size: " + maxs);
 		mins = Configuration.getInt(prefix + "." + PAR_MIN_SIZE, 0);
+		MTU = Configuration.getInt(prefix + "." + PAR_MTU);
+
 	}
 
 	@Override
 	public boolean execute() {
 		long time = CommonState.getTime();
-		long endtime = CommonState.getEndTime();
+		if (time == 0) return false;
 
-		if (time < endtime / 3) {
-			exec = !exec;
-		} else if (time < 2*endtime/3) {
-			exec = true;
-		} else {
-			exec = false;
+		// Leave hard coded for now, considering config parameters
+		if (time >= 15*1000) {
+			Scuttlebutt.MTU = this.MTU;
 		}
-		
-		if (exec) {
-			for (int i = 0; i < N; i++) {
-				Node node = Network.get(i);
-				((Scuttlebutt) node.getProtocol(pid)).updateSelf((int) node.getID(), CommonState.r.nextInt(K),
-					mins + 1 + CommonState.r.nextInt(maxs-1));
-			}
+
+		if (time < 25) {
+			updateAll();
+		} else if (time < 75) {
+			updateAll();
+			updateAll();
+		} else if (time < 120) {
+			updateAll();
 		}
-		
 		return false;
+	}
+
+	private void updateAll() {
+		for (int i = 0; i < N; i++) {
+			Node node = Network.get(i);
+			((Scuttlebutt) node.getProtocol(pid)).updateSelf((int) node.getID(), CommonState.r.nextInt(K),
+					mins + 1 + CommonState.r.nextInt(maxs-1));
+		}
 	}
 
 }
