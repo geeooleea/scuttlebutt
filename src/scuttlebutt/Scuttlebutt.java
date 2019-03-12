0aa81948001e3cbf2aad71a04759a84cfab7a713
geeooleea <giulia.carocari@studenti.unitn.it>
2019-03-12 17:24:09 +0100

    Add scuttle-depth ordering (to be fixed)

--- src/scuttlebutt/Scuttlebutt.java
+++ src/scuttlebutt/Scuttlebutt.java
@@ -15,6 +15,8 @@ import peersim.core.Node;
 import peersim.edsim.EDProtocol;
 import peersim.edsim.NextCycleEvent;
 import peersim.transport.Transport;
+
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
@@ -139,147 +141,23 @@ public class Scuttlebutt extends NextCycleEvent implements EDProtocol, CDProtoco
 		db.updateSelf(node, key, value);
 	}
 
-	/**
-	 *
-	 * @param digest
-	 * @return
-	 */
 	private DeltaSet scuttleDepth(int[] digest) {
-		int values[][] = new int[N][K];
-		int timestamps[][] = new int[N][K];
-		int pos[] = new int[N];
-		int node[] = new int[N];
-		int priority[] = new int[N];
+		PriorityQueue<Delta> deltas[] = getDeltas(digest);
+		DeltaSet deltaSet = new DeltaSet(N,K);
 
-		init(pos, node, priority);
-
-		// Obtain all deltas that are more recent than the peer's maximum timestamp in the digest
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < K; j++) {
-				int time = db.getTimestamp(i, j);
-				if (digest[i] >= 0 && digest[i] < time) {
-					values[i][j] = db.getValue(i, j);
-					timestamps[i][j] = time;
-					increase(pos, node, priority, i);
-				}
+		Arrays.sort(deltas, new ScuttleDepthComparator());
+		for (int i=0; i<N && deltaSet.entryNumber() < MTU; i++) {
+			while (!deltas[i].isEmpty() && deltaSet.entryNumber() < MTU) {
+				Delta d = deltas[i].poll();
+				deltaSet.put(d.node,d.key,d.value,d.timestamp);
 			}
 		}
-		DeltaSet deltaSet = new DeltaSet(N, K);
 
-		for (int i = 0; i < N; i++) {
-			int n = deleteMax(node, pos, priority, N - i);
-			for (int j = 0; j < K; j++) {
-				int maxTimestamp = 0, maxKey = 0;
-				for (int k = 0; k < K; k++) {
-					if (values[n][k] > 0 && timestamps[n][k] > maxTimestamp) {
-						maxTimestamp = timestamps[n][k];
-						maxKey = k;
-					}
-				}
-				if (DELTA_HEADER * (deltaSet.entryNumber() + 1) + deltaSet.size() + values[n][maxKey] <= MTU) {
-					deltaSet.put(n, maxKey, values[n][maxKey], maxTimestamp);
-				}
-				if (DELTA_HEADER * deltaSet.entryNumber() + deltaSet.size() == MTU) {
-					return deltaSet;
-				}
-				// No longer considering this entry, independently of insertion
-				values[n][maxKey] = 0;
-				timestamps[n][maxKey] = 0;
-			}
-		}
 		return deltaSet;
 	}
 
-	private static int p(int i) {
-		return (i - 1)/2;
-	}
-
-	private static int l(int i) {
-		return (2 * i + 1);
-	}
-
-	private static int r(int i) {
-		return (2 * i + 2);
-	}
-
-	private void init(int[] pos, int node[], int priority[]) {
-		for (int i = 0; i < N; i++) {
-			pos[i] = i;
-			node[i] = i;
-			priority[i] = 0;
-		}
-	}
-
-	/**
-	 * Increases the priority of the node by 1.
-	 *
-	 * @param pos
-	 * @param priority
-	 * @param node
-	 */
-	private static void increase(int[] pos, int node[], int[] priority, int n) {
-		int i = pos[n];
-		priority[i]++;
-		while (i > 0 && priority[i] > priority[p(pos[i])]) {	
-			swap(priority, i, p(i));
-			swap(pos, i, p(i));
-			swap(node, i, p(i));
-			i = p(i);
-		}
-	}
-
-	/**
-	 * @param node
-	 * @param pos
-	 * @param priority
-	 * @param i
-	 * @param dim
-	 */
-	private static void maxHeapRestore(int node[], int[] pos, int[] priority, int i, int dim) {
-		int maxindex;
-		do {
-			maxindex = i;
-			if (l(i) <= dim && priority[l(i)] > priority[maxindex]) {
-				maxindex = l(i);
-			}
-			if (r(i) <= dim && priority[r(i)] > priority[maxindex]) {
-				maxindex = r(i);
-			}
-			if (maxindex != i) {
-				swap(priority, i, maxindex);
-				swap(pos, i, maxindex);
-				swap(node, i, maxindex);
-			}
-		} while (maxindex != i);
-	}
-
-	private static int deleteMax(int node[], int pos[], int heap[], int dim) {
-		swap(heap, 0, dim - 1);
-		swap(pos, 0, dim - 1);
-		swap(node, 0, dim - 1);
-		dim--;
-		maxHeapRestore(node, pos, heap, 0, dim - 1);
-		return node[dim];
-	}
-
-	private static void swap(int[] arr, int i, int j) {
-		arr[i] = (arr[i] + arr[j]) - (arr[j] = arr[i]);
-	}
-
 	private DeltaSet scuttleBreadth(int[] digest) {
-		PriorityQueue<Delta> deltas[] = new PriorityQueue[N];
-		DeltaComparator cmp = new DeltaComparator();
-
-		// Obtain all deltas that are more recent than the peer's maximum timestamp in the digest
-		for (int i = 0; i < N; i++) {
-			deltas[i] = new PriorityQueue<>(K,cmp);
-			for (int j = 0; j < K; j++) {
-				int time = db.getTimestamp(i, j);
-				if (digest[i] >= 0 && digest[i] < time) {
-					deltas[i].add(new Delta(j,time,db.getValue(i,j)));
-				}
-			}
-		}
+		PriorityQueue<Delta> deltas[] = getDeltas(digest);
 
 		boolean empty = false;
 		DeltaSet deltaSet = new DeltaSet(N,K);
@@ -307,13 +185,40 @@ public class Scuttlebutt extends NextCycleEvent implements EDProtocol, CDProtoco
 		}
 	}
 
+	class ScuttleDepthComparator implements Comparator<PriorityQueue<Delta>>{
+		@Override
+		public int compare(PriorityQueue<Delta> deltas, PriorityQueue<Delta> t1) {
+			if (deltas.size() > t1.size()) return -1;
+			else if (deltas.size() == t1.size()) return 0;
+			else return 1;
+		}
+	}
+
 	private class Delta {
-		int key, timestamp, value;
-		public Delta(int key, int timestamp, int value) {
+		int node, key, timestamp, value;
+		public Delta(int node, int key, int timestamp, int value) {
+			this.node = node;
 			this.key = key;
 			this.timestamp = timestamp;
 			this.value = value;
 		}
+	}
+
+	private PriorityQueue<Delta>[] getDeltas(int[] digest) {
+		PriorityQueue<Delta> deltas[] = new PriorityQueue[N];
+		DeltaComparator cmp = new DeltaComparator();
+
+		// Obtain all deltas that are more recent than the peer's maximum timestamp in the digest
+		for (int i = 0; i < N; i++) {
+			deltas[i] = new PriorityQueue<>(K,cmp);
+			for (int j = 0; j < K; j++) {
+				int time = db.getTimestamp(i, j);
+				if (digest[i] >= 0 && digest[i] < time) {
+					deltas[i].add(new Delta(i,j,time,db.getValue(i,j)));
+				}
+			}
+		}
+		return deltas;
 	}
 
 	@Override
